fa5fe5ace5c868990eed1b0dc0c6cf2408cc53e7
Jeremybbl <jeremy.boubal@gmail.com>
2020-05-10 22:27:31 +0200

    dragAndScroll working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 app = {
 
   init:function(){
-    console.log("coucou")
+    app.dragToScroll();
   },
 
   // Scroll to anchors
@@ -18,8 +18,40 @@ app = {
     inc = (box.top >= 0) ? 1 : -1;
     for (k = 0; k < 49; k++) setTimeout("window.scrollBy(0," + Math.floor(box.top / 50) + ")", 10 * k);
     setTimeout("app.myLastScrollTo('" + id + "')", 500);
+  },
+
+  dragToScroll: function(){
+    const slider = document.querySelector('.project__list');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
   }
 }
+
+
 
 app.init();
 
