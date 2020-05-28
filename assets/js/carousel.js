ebda54262a4eec65a87a7c13e9fb1a54553c0f9e
Jeremybbl <jeremy.boubal@gmail.com>
2020-05-28 22:29:53 +0200

    carousel project working without pagination and touch sensitive

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,25 +1,38 @@
 class Carousel {
 
 
+
+  /**
+   * 
+   * @callback moveCallback 
+   * @param {number} index
+   */
+
   /**
    * 
    * @param {HTMLElement} element 
    * @param {Object} options
-   * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
-   * @param {Object} options.slidesVisible Nombre d'éléments visibles dans un slide
+   * @param {Object} [options.slidesToScroll=1] Nombre d'éléments à faire défiler
+   * @param {Object} [options.slidesVisible=1] Nombre d'éléments visibles dans un slide
+   * @param {boolean} [options.loop=false] Doit-on boucler en fin de slides
    */
   constructor(element, options = {}) {
     this.element = element;
     this.options = Object.assign({}, {
         slidesToScroll: 1,
-        slidesVisible: 1
+        slidesVisible: 1,
+        loop: false
       },
       options);
     let children = [].slice.call(element.children);
+    this.isMobile = false;
     this.currentItem = 0;
+    this.moveCallbacks = [];
+
+    // DOM alteration
     this.root = this.createDivWithClass("carousel");
     this.container = this.createDivWithClass("carousel__container");
-
+    this.root.setAttribute('tabindex', '0');
     this.root.appendChild(this.container);
     this.element.appendChild(this.root);
     this.items = children.map((child) => {
@@ -30,15 +43,27 @@ class Carousel {
     });
     this.setStyle();
     this.createNavigation();
+
+    // Events
+    this.moveCallbacks.forEach(cb => cb(0));
+    this.onWindowResize();
+    window.addEventListener('resize', this.onWindowResize.bind(this));
+    this.root.addEventListener('keyup', e => {
+      if (e.key === 'ArrowRight' || e.key === "Right") {
+        this.next();
+      } else if (e.key === 'ArrowLeft' || e.key === "Left") {
+        this.prev();
+      }
+    })
   }
 
   /**
    * 
    */
   setStyle() {
-    let ratio = this.items.length / this.options.slidesVisible;
+    let ratio = this.items.length / this.slidesVisible;
     this.container.style.width = (ratio * 100) + "%";
-    this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%");
+    this.items.forEach(item => item.style.width = ((100 / this.slidesVisible) / ratio) + "%");
   }
 
 
@@ -48,15 +73,31 @@ class Carousel {
     this.root.appendChild(nextButton);
     this.root.appendChild(prevButton);
     nextButton.addEventListener('click', this.next.bind(this));
+    prevButton.addEventListener('click', this.prev.bind(this));
+    if (this.options.loop === true) {
+      return;
+    }
+    this.onMove(index => {
+      if (index === 0) {
+        prevButton.classList.add('carousel__prev--hidden');
+      } else {
+        prevButton.classList.remove('carousel__prev--hidden');
+      }
+      if (this.items[this.currentItem + this.slidesVisible] === undefined) {
+        nextButton.classList.add('carousel__next--hidden');
+      } else {
+        nextButton.classList.remove('carousel__next--hidden');
+      }
+    })
   }
 
 
   next() {
-    this.goToItem(this.currentItem + this.options.slidesToScroll)
+    this.goToItem(this.currentItem + this.slidesToScroll);
   }
 
   prev() {
-    this.goToItem(this.currentItem - this.options.slidesToScroll)
+    this.goToItem(this.currentItem - this.slidesToScroll);
   }
 
   /**
@@ -64,12 +105,45 @@ class Carousel {
    * @param {number} index
    */
   goToItem(index) {
-    let translateX = index * 100 / this.items.length
-    this.container.style.transform = "translate3d(" + translateX + ", 0, 0)"
+
+    if (index < 0) {
+
+      if (this.options.loop) {
+        index = this.items.length - this.slidesVisible;
+      } else {
+        return;
+      }
+      
+    } else if (index >= this.items.length || (this.items[this.currentItem + this.slidesVisible] === undefined && index > this.currentItem)) {
+
+      if (this.options.loop) {
+        index = 0;
+      } else {
+        return;
+      }
+    }
+    let translateX = index * -100 / this.items.length
+    this.container.style.transform = "translate3d(" + translateX + "%, 0, 0)"
     this.currentItem = index;
+    this.moveCallbacks.forEach(cb => cb(index));
   }
 
+  /**
+   * 
+   * @param {moveCallback} cb 
+   */
+  onMove(cb) {
+    this.moveCallbacks.push(cb);
+  }
 
+  onWindowResize() {
+    let mobile = window.innerWidth < 800;
+    if (mobile !== this.isMobile) {
+      this.isMobile = mobile;
+      this.setStyle();
+      this.moveCallbacks.forEach(cb => cb(this.currentItem));
+    }
+  }
   /**
    * 
    * @param {string} className
@@ -80,12 +154,27 @@ class Carousel {
     div.setAttribute('class', className)
     return div;
   }
+
+  /**
+   * @returns (number)
+   */
+  get slidesToScroll() {
+    return this.isMobile ? 1 : this.options.slidesToScroll;
+  }
+
+  /**
+   * @returns (number)
+   */
+  get slidesVisible() {
+    return this.isMobile ? 1 : this.options.slidesVisible;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 
   new Carousel(document.getElementById('project__carousel'), {
-    slidesToScroll: 3,
-    slidesVisible: 3
+    slidesToScroll: 1,
+    slidesVisible: 2,
+    loop: false
   })
 })
